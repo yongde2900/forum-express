af9d4a9f6375b4734a9b99027c557776aedcb049
yongde2900 <com414141@gmail.com>
2020-11-26 23:09:15 +0800

    feat: add confirm user password and unique email feature

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const db =require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}) )
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({secret: 'secret', resave: false, saveUninitialized:false}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,13 +7,27 @@ let userController = {
         res.render('signup')
     },
     signUp: (req, res) => {
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-        }).then(user => {
-            return res.redirect('/signin')
-        })
+        const {email, name, password, passwordCheck} = req.body
+        if(passwordCheck !== password){
+            req.flash('error_msg', '兩次密碼輸入不同')
+            return res.render('signup', {email, name})
+        }else{
+            User.findOne({where: {email: email}})
+                .then(user => {
+                    if(user) {
+                        req.flash('error_msg', '信箱重複')
+                        return res.render('signup', {email, name})
+                    }else {
+                        User.create({
+                            name: name,
+                            email: email,
+                            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+                        }).then(user => {
+                            return res.redirect('/signin')
+                        })
+                    }
+                })
+        }
     }
 }
 
