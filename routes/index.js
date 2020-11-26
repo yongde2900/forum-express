2d86657cc37f1b3083d2735af22d0ab5a93b45cd
yongde2900 <com414141@gmail.com>
2020-11-26 22:39:29 +0800

    feat: add userComtoleer amd signup page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userControllers')
+const { use } = require('chai')
 
 module.exports = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
-
   app.get('/restaurants', restController.getRestaurants)
 
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
 
