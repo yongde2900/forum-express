380bc2ffbc07ca9b206fce7e14d430f824949d1b
yongde2900 <com414141@gmail.com>
2020-11-28 16:20:29 +0800

    feat: add admin restaurant page

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurants)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
 
 
   app.get('/signup', userController.signUpPage)
