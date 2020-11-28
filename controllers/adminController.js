3c277a7e4faa738c4c8e7dc19cfb1ff2d32bb6a7
yongde2900 <com414141@gmail.com>
2020-11-28 16:17:29 +0800

    feat: add admin restaurant create featur

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,9 +3,30 @@ const Restaurant = db.Restaurant
 
 let adminController = {
     getRestaurants: (req, res) => {
-        return Restaurant.findAll({raw: true, nest: true})
+        return Restaurant.findAll({raw: true})
             .then(restaurants => {
                 return res.render('admin/restaurants', {restaurants})
+            })
+    },
+    createRestaurants: (req, res) => {
+        return res.render('admin/create')
+    },
+    postRestaurant: (req, res) => {
+        const{name, tel, address, opening_hours, description} =req.body
+        if(!req.body.name){
+            req.flash('error_msg', "Name didn't exist")
+            return res.redirect('back')
+        }
+        return Restaurant.create({name, tel, address, opening_hours, description})
+            .then(restaurant => {
+                req.flash('success_msg', 'restaurant was successfully created!')
+                res.redirect('/admin/restaurants')
+            })
+    },
+    getRestaurant: (req, res) => {
+        return Restaurant.findByPk(req.params.id,{raw: true})
+            .then(restaurant => {
+                return res.render('admin/restaurant', {restaurant})
             })
     }
 }
