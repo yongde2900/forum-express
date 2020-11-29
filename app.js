b3a6ad291adabcb2e91719146556ece81939230c
yongde2900 <com414141@gmail.com>
2020-11-29 16:43:38 +0800

    modify dotenv

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
 if(process.env.NODE_ENV !== 'production') {
-  require('dot-env').config()
+  require('dotenv').config()
 }
 
 app.use((req, res, next) => {
