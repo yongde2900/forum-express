ba8f3b7ae8162683cf17d51c3db1f481f24ee638
yongde2900 <com414141@gmail.com>
2020-12-02 20:44:09 +0800

    feat: add edit category feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,17 +1,31 @@
 const db = require('../models')
-const category = require('../models/category')
 const Category = db. Category
 let categoryController = {
     getCategories: (req, res) => {
         Category.findAll({raw: true})   
         .then(categories => {
-            res.render('admin/category/categories', {categories})
+            if(req.params.id) {
+                category = categories.find(obj => obj.id == req.params.id)
+                res.render('admin/category/categories', {categories, category})
+            }else{
+                res.render('admin/category/categories', {categories})
+            }
         })
     },
     postCategory: (req, res) => {
         Category.create({name: req.body.name})
             .then(category => {
                 req.flash('success_msg', 'Category was successfully created !')
+                res.redirect('/admin/categories')
+            })
+    },
+    putCategory: (req, res) => {
+        Category.findByPk(req.params.id)
+            .then(category => {
+                category.update({name: req.body.name})
+            })
+            .then(() => {
+                req.flash('success_msg', 'Category was successfully updated !')
                 res.redirect('/admin/categories')
             })
     }
