744c53e5007892cc428021e93793dce3350cb5c4
yongde2900 <com414141@gmail.com>
2020-12-02 17:33:57 +0800

    update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/upload', express.static(__dirname + '/upload'))
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}) )
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs', helpers: require('./config/handlebars-helpers')}) )
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({secret: 'secret', resave: false, saveUninitialized:false}))
