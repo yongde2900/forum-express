a5fa799b1b63c0aa2229915b4dc3c5c743d5d566
yongde2900 <com414141@gmail.com>
2020-12-03 11:13:58 +0800

    feat: add ategories navbar on restaurants page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,25 +1,35 @@
-const db =require('../models')
+const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 let restController = {
     getRestaurants: (req, res) => {
+        let whereQuery = {}
+        let categoryId = ''
+        if (req.query.categoryId) {
+            categoryId = Number(req.query.categoryId)
+            whereQuery.categoryId = categoryId
+        }
         Restaurant.findAll({
             raw: true,
             nest: true,
-            include: [Category]
+            include: [Category],
+            where: whereQuery
         })
-        .then(restaurants => {
-            const data = restaurants.map(r => ({
-                ...r,
-                description: r.description.substring(0,50)
-            }))
-            res.render('restaurants', {restaurants: data})
-        })
+            .then(restaurants => {
+                Category.findAll({raw: true})
+                .then(categories => {
+                    const data = restaurants.map(r => ({
+                        ...r,
+                        description: r.description.substring(0, 50)
+                    }))
+                    res.render('restaurants', { restaurants: data,categories, categoryId })
+                })
+            })
     },
     getRestaurant: (req, res) => {
-        Restaurant.findByPk(req.params.id, {include: [Category]})
+        Restaurant.findByPk(req.params.id, { include: [Category] })
             .then(restaurant => {
-                res.render('restaurant', {restaurant: restaurant.toJSON()})
+                res.render('restaurant', { restaurant: restaurant.toJSON() })
             })
     }
 }
